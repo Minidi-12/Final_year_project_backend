2909b04ffa3219645786369a931b6114d131a911
Minidi <sathpiyumisam2003@gmail.com>
2026-05-07 17:54:12 +0530

    Automated WhatsApp Notification System using Twilio

--- src/api/User.ts
+++ src/api/User.ts
@@ -1,14 +1,9 @@
 import express from "express";
 import { login } from "../application/User";
-import UserModel from "../infrastructure/db/entities/User"; 
-
 const router = express.Router();
 
 router.post("/login", login);
 
-router.get("/users", async (req, res) => {
-  const users = await UserModel.find({}, { password: 0 });
-  res.json(users);
-});
+
 
 export default router;
