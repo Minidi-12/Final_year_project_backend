96280208bb564536745e8b2f7a036ebd3920574b
Minidi <sathpiyumisam2003@gmail.com>
2026-05-25 23:01:37 +0530

    deploy

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import { startRiskAlertScheduler } from './application/riskAlert.schedular';
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://h-connect.netlify.app',   
+  origin: 'https://hope-connect-vert.vercel.app',   
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE','OPTIONS'],  
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
