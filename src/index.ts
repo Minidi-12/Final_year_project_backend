39d769a6ee7aa2c716b1f777aa196756d4db853f
Minidi <sathpiyumisam2003@gmail.com>
2026-05-25 12:13:40 +0530

    option

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: 'https://h-connect.netlify.app',   
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],  
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE','OPTIONS'],  
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }))
